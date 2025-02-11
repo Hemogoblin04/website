f6ea0887eba3850cd626ad9f7466ab286070d903
Hector Mauricio <hem@njit.edu>
2025-02-11 13:41:59 -0500

    script.js updated

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const galleryImages = documentGetElementById("galleryImg");
 const nextBtn = documentGetElementById("nextBtn");
 const prevBtn = documentGetElementById("nextBtn")
 
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    galleryImg.src = images[currentIndex];
+});
+
+nextBtn.addEventListener("click", () => {
+currentIndex = (currentIdnex + 1 +images.length) %images.length;
+galleryImg.src = images[currentIndex];
+});
