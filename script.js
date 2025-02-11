3738416a852e3aceb480d846b1187c88e545c41f
Hector Mauricio <hem@njit.edu>
2025-02-11 13:32:58 -0500

    Index html

---
+++ script.js
@@ -0,0 +1,7 @@
+const images = ["images/latte1.png","images/latte2.png","images/latte3.png"];
+let currentIndex = 0;
+
+const galleryImages = documentGetElementById("galleryImg");
+const nextBtn = documentGetElementById("nextBtn");
+const prevBtn = documentGetElementById("nextBtn")
+
